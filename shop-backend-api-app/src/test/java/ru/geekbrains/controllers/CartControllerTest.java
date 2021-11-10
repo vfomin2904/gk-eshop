584847dd766f129755032c0e4b1e9b2067e338cf
vfomin2904 <vova-fomin97@mail.ru>
2021-11-10 21:39:06 +0300

    hw 12 junit

--- shop-backend-api-app/src/test/java/ru/geekbrains/controllers/CartControllerTest.java
+++ shop-backend-api-app/src/test/java/ru/geekbrains/controllers/CartControllerTest.java
@@ -2,6 +2,7 @@ package ru.geekbrains.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -12,14 +13,21 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.test.context.support.WithSecurityContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.geekbrains.controller.dto.AddLineItemDto;
+import ru.geekbrains.persist.ProductRepository;
+import ru.geekbrains.persist.model.Category;
+import ru.geekbrains.persist.model.Product;
 import ru.geekbrains.service.CartService;
 import ru.geekbrains.service.ProductService;
+import org.springframework.security.test.context.support.WithMockUser;
+import ru.geekbrains.service.dto.LineItem;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -34,6 +42,9 @@ public class CartControllerTest {
     @Autowired
     CartService cartService;
 
+    @Autowired
+    ProductRepository productRepository;
+
     @MockBean
     private RabbitTemplate rabbitTemplate;
 
@@ -43,7 +54,7 @@ public class CartControllerTest {
     ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    public void addToCart() throws Exception {
+    public void addToCartForriben() throws Exception {
         AddLineItemDto addLineItemDto = new AddLineItemDto();
         addLineItemDto.setColor("red");
         addLineItemDto.setMaterial("wood");
@@ -54,7 +65,34 @@ public class CartControllerTest {
         mvc.perform(MockMvcRequestBuilders
         .post("/cart")
         .contentType(MediaType.APPLICATION_JSON)
-        .param("addLineItemDto", objectMapper.writeValueAsString(addLineItemDto)))
+        .content(objectMapper.writeValueAsString(addLineItemDto)))
                 .andExpect(status().isForbidden());
+    }
+
+    @WithMockUser(value = "admin", password = "admin", roles = {"ADMIN"})
+    @Test
+    public void addToCart() throws Exception {
+        Product product = new Product();
+        product.setName("Test product");
+        product.setId(1L);
+        product.setCategory(new Category(1L, "test"));
+        productRepository.save(product);
+
+        AddLineItemDto addLineItemDto = new AddLineItemDto();
+        addLineItemDto.setColor("red");
+        addLineItemDto.setMaterial("wood");
+        addLineItemDto.setProductId(1L);
+        addLineItemDto.setQty(10);
+
+        mvc.perform(MockMvcRequestBuilders
+                .post("/cart")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(addLineItemDto)))
+                .andExpect(status().isOk());
+
+        LineItem item = cartService.getLineItems().stream().filter(lineItem -> lineItem.getProductId() == 1L).findFirst().orElse(null);
+
+        Assertions.assertNotNull(item);
     }
 }
